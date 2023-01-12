6d24a8df4091d861ee3beefffb1451b21dbf26d1
Nicholas Caputo <nick.caputo12@gmail.com>
2023-01-12 13:27:52 -0800

    commit tree 9f8679c129f0c9033916059e363f02d0d57e9ca9

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    return cats.push("Ralph")
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(name) {
+    return cats.pop("Garfield")
+}
+function destructivelyRemoveFirstCat (name) {
+    return cats.shift("Milo")
+}
+function appendCat(name) {
+    const newCats= [...cats, "Broom"]
+    return newCats
+}
+function prependCat(name) {
+    const newCatsTwo = ["Arnold", ...cats,]
+    return newCatsTwo
+}
+function removeLastCat (name) {
+    const newCatsThree = cats.slice(0,2)
+    return newCatsThree
+}
+function removeFirstCat(name) {
+    const newCatsFour = cats.slice(1)
+    return newCatsFour
+}
